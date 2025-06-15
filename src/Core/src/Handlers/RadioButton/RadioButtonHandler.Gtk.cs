ce1c06f1f05422a70c150276a82ad88d3b572198
Lemon73 <108001913+Lemon73-Computing@users.noreply.github.com>
2025-06-15 14:09:02 +0900

    Core,Controls(Gtk): Implement RadioButton

--- src/Core/src/Handlers/RadioButton/RadioButtonHandler.Gtk.cs
+++ src/Core/src/Handlers/RadioButton/RadioButtonHandler.Gtk.cs
@@ -1,19 +1,73 @@
-﻿using System;
+﻿using Gtk;
+using System;
 
 namespace Microsoft.Maui.Handlers
 {
-	public partial class RadioButtonHandler : ViewHandler<IRadioButton, NotImplementedView>
+	public partial class RadioButtonHandler : ViewHandler<IRadioButton, RadioButton>
 	{
-		protected override NotImplementedView CreatePlatformView() => new(nameof(IRadioButton));
+		static RadioButton baseRadioButton = new("base");
 
+		protected override RadioButton CreatePlatformView()
+		{
+			return new RadioButton(baseRadioButton, "foo");
+		}
+
+		protected override void ConnectHandler(RadioButton platformView)
+		{
+			platformView.Toggled += OnClicked;
+			// base.ConnectHandler(platformView);
+		}
+
+		protected override void DisconnectHandler(RadioButton platformView)
+		{
+			platformView.Toggled -= OnClicked;
+			// base.DisconnectHandler(platformView);
+		}
+
+		[MissingMapper]
 		public static void MapBackground(IRadioButtonHandler handler, IRadioButton radioButton) { }
-		public static void MapIsChecked(IRadioButtonHandler handler, IRadioButton radioButton) { }
-		public static void MapContent(IRadioButtonHandler handler, IRadioButton radioButton) { }
-		public static void MapTextColor(IRadioButtonHandler handler, ITextStyle textStyle) { }
+
+		public static void MapIsChecked(IRadioButtonHandler handler, IRadioButton radioButton)
+		{
+			if (handler.PlatformView is RadioButton rb)
+				rb.UpdateIsChecked(radioButton);
+			// handler.PlatformView?.UpdateIsChecked(radioButton);
+		}
+
+		public static void MapContent(IRadioButtonHandler handler, IRadioButton radioButton)
+		{
+			if (handler.PlatformView is RadioButton rb)
+				rb.UpdateContent(radioButton);
+			// handler.PlatformView?.UpdateContent(radioButton);
+		}
+
+		public static void MapTextColor(IRadioButtonHandler handler, ITextStyle textStyle)
+		{
+			handler.PlatformView?.UpdateTextColor(textStyle.TextColor);
+		}
+
+		[MissingMapper]
 		public static void MapCharacterSpacing(IRadioButtonHandler handler, ITextStyle textStyle) { }
-		public static void MapFont(IRadioButtonHandler handler, ITextStyle textStyle) { }
+
+		public static void MapFont(IRadioButtonHandler handler, ITextStyle textStyle)
+		{
+			var fontManager = handler.GetRequiredService<IFontManager>();
+			handler.PlatformView?.UpdateFont(textStyle, fontManager);
+		}
+
+		[MissingMapper]
 		public static void MapStrokeColor(IRadioButtonHandler handler, IRadioButton radioButton) { }
+
+		[MissingMapper]
 		public static void MapStrokeThickness(IRadioButtonHandler handler, IRadioButton radioButton) { }
+
+		[MissingMapper]
 		public static void MapCornerRadius(IRadioButtonHandler handler, IRadioButton radioButton) { }
+
+		void OnClicked(object? sender, EventArgs e)
+		{
+			if (sender is RadioButton platformView && VirtualView != null)
+				VirtualView.IsChecked = platformView.Active;
+		}
 	}
 }
